586ff86993c473a685e97647c00a43f533dd082d
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-11-04 15:50:44 +0200

    added tkinter files browsing

--- subtitles-modify-py.py
+++ subtitles-modify-py.py
@@ -1,12 +1,11 @@
 import srt
 import tkinter as tk
 from datetime import timedelta
+from tkinter import filedialog
+import os
 
 root = tk.Tk()
 root.title("Subtitles Timestamp Modify")
-
-entry = tk.Entry(root)
-entry.pack()
 
 
 def is_not_number(input_str):
@@ -36,25 +35,50 @@ def adjust_subtitles(input_file, output_file, time_adjustment_seconds):
 
         print(
             f"Subtitles adjusted by {time_adjustment_seconds} seconds and saved to {output_file}")
-
+        timeEntry.delete(0, tk.END)
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
+def srt_file_browsing():
+    global input_file
+    input_file = filedialog.askopenfilename(
+        filetypes=[("SRT files", "*.srt")])
+    if (input_file):
+
+        input_file_name = os.path.basename(input_file)
+        fileName_Label.config(text=input_file_name)
+        return input_file
+    return False
+
+
 def get_time_adjustment_and_adjust():
-    time_adjustment_str = entry.get()
+    time_adjustment_str = timeEntry.get()
 
     if is_not_number(time_adjustment_str):
         print("Invalid input. Please enter a valid number.")
     else:
         time_adjustment_seconds = float(time_adjustment_str)
 
-        input_file = "fresh.off.the.boat.1e04.dvdrip.x264-reward.srt"
-        output_file = "output.srt"
-        adjust_subtitles(input_file, output_file, time_adjustment_seconds)
+        if input_file:
+            output_file = filedialog.asksaveasfilename(
+                defaultextension=".srt", filetypes=[("SRT files", "*.srt")])
+
+            if output_file:
+                adjust_subtitles(input_file, output_file,
+                                 time_adjustment_seconds)
 
 
-adjust_button = tk.Button(root, text="Adjust Subtitles",
+Browsebtn = tk.Button(root, text="Browse srt file",
+                      command=srt_file_browsing)
+Browsebtn.pack()
+fileName_Label = tk.Label(root, text='')
+fileName_Label.pack()
+
+timeEntry = tk.Entry(root)
+timeEntry.pack()
+
+adjust_button = tk.Button(root, text="Adjust and save",
                           command=get_time_adjustment_and_adjust)
 adjust_button.pack()
 
